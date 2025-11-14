00e73fd02b209e3403bb34bf15b4fb306e361c6b
Noah Harman <noahharman190@gmail.com>
2025-11-14 12:35:14 -0500

    Nov 14

--- src/main_game.v
+++ src/main_game.v
@@ -4,19 +4,19 @@ module main_game #(
 )(
     input clk,
     input sw1,
-    input sw1,
-    input sw1,
-    input sw1,
+    input sw2,
+    input sw3,
+    input sw4,
     output d1,
     output d2,
     output d3,
     output d4,
-    output reg [3:0] score
+    output reg [3:0] o_score
 );
 
 reg  [2:0] r_sm_main;
 reg  rsw1, rsw2, rsw3, rsw4;
-reg  [1:0] pattern [0:10];
+reg  [1:0] r_pattern [0:10];
 reg  r_toggle, r_button_dv;
 wire [21:0] lfsr_data;
 reg  [$clog2(game_limit) - 1:0] r_index;
@@ -39,7 +39,7 @@ always @(posedge clk) begin
             start:           //reset cleared -> pattern off
             begin 
                 if (!i_switch_1 & !i_switch_2 & r_button_dv) begin 
-                    score <= 0; 
+                    o_score <= 0; 
                     r_index <= 0; 
                     r_sm_main <= pattern_off; 
                 end
@@ -62,8 +62,27 @@ always @(posedge clk) begin
                     end 
                 end 
             end 
-            wait_player:   //incorect pattern -> loser, correct not complete -> wait player, pattern complete -> incr score
-            incr_score:    //score not at limit -> pattern off, game over -> winner
+            wait_player:  //incorect pattern -> loser, correct not complete -> wait player, pattern complete -> incr score
+            begin
+                if(r_button_dv)   //Indicates player has pressed and released a switch 
+                    if(r_pattern[r_index] == r_Button _ID && r_index == o_score) begin //Checks if button is corret and we are at the end of the pattern
+                            r_index <= 0; 
+                            r_sm_main <= o_score; 
+                        end 
+                    else if (r_pattern[r_index] != r_button_ID) //Checks if button that was pressed is supposed to be pressed
+                        r_sm_main <= loser; 
+                    else
+                        r_index <= r_index + 1; 
+            end
+            incr_score:  //score not at limit -> pattern off, game over -> winner
+            begin
+                o_score < o_score  + 1;
+                if(o_score == game_limit - 1)
+                    if(o_score == game_limit)
+                        r_sm_main <= winner;
+                    else
+                        r_sm_main <= pattern_off;
+            end
             loser:
             winner:
         endcase 
