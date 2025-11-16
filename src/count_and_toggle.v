61cd79af7428344f89e68be17737aaa3f767898c
Noah Harman <noahharman190@gmail.com>
2025-11-15 23:59:19 -0500

    Nov 14

--- src/count_and_toggle.v
+++ src/count_and_toggle.v
@@ -8,12 +8,12 @@ module count_and_toggle #(count_limit = 10)
 reg [$clog2(count_limit):0] r_counter;
 
 always @(posedge i_clk) begin 
-    if(i_enable == 1'b1) begin
-        if(r_counter == count_limit -1) begin 
+    if(i_enable == 1'b1) begin //if enable is active begin counting
+        if(r_counter == count_limit -1) begin   
             o_toggle <= 1'b1;
             r_counter <= 0;
         end
-        else r_counter <= r_counter + 1; 
+        else r_counter <= r_counter + 1;  //if not at limit keep counting
     end 
     else begin
         o_toggle <= 1'b0;
